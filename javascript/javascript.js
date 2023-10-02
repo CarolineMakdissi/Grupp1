69795a638cd29b9f11ff24531018eb23121e2f90
zainabfarah <zainabfarah890@gmail.com>
2023-10-02 06:52:13 +0200

    ändring

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,18 @@
 console.log("Hejsan internt javascript");
+
+//för att kunna identifiera HTML-element från body måste sidan läsas
+window.onload = function
+//identifierar ett HTML-element
+minParagraf = dokument.getElementbyId("min-paragraf");
+console.log(minParagraf);
+//ändrar stilsättning på HTML-element
+minParagra.style.colo = "red";
+
+//identifierar ett annat HTML-element 
+klickbarparagraf = dokument.getElementbyId("klickbar-paragraf");
+//lägga till en händelsehanterate till detta element
+klickbarparagraf.addEventlistener("click",function) {
+    console.log(klickbarparagraf);
+    //ändrar stilsättning på det klickade HtML-elementet
+    klickbarparagraf.style.color = "blue";
+};
